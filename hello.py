b1fa99a722bba9fd16ee30c095d3ab7878a8ef9e
Khuyagbaatar Batsuren <50955407+kbatsuren@users.noreply.github.com>
2019-05-31 14:16:17 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,23 +1,4 @@
-from wiktionaryparser import WiktionaryParser
 import Wiktra as wt
 print('Hello World!')
 
-# parser = WiktionaryParser()
-#
-# parser.set_default_language('vietnamese')
-#
-# parser.include_relation('alternative forms')
-#
-# word = parser.fetch('bôm')
-#
-# print(word)
-#
-#
-# word = parser.fetch('test')
-# another_word = parser.fetch('test', 'french')
-#
-# print(word)
-#
-# print(another_word)
-
-print(wt.translite('монгол улс мандтугай', 'mongolian'))
+print(wt.translite('монгол улс мандтугай', 'mon'))
