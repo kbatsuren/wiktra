508787019fdab264309e8e5c6e24875ee62e4fe9
Khuyagbaatar Batsuren <50955407+kbatsuren@users.noreply.github.com>
2021-03-18 06:23:30 +0800

    Setup file for pip
    This file should be updated soon.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='wiktra',
+   version='1.0',
+   description='Wiktionary Transiltration tool',
+   author='Khuyagbaatar Batsuren',
+   author_email='khuyagbaatar@gmail.com',
+   packages=['wiktra'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
