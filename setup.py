23370fb5f61c43070b3c1a3405d95c16d2227790
Khuyagbaatar Batsuren <50955407+kbatsuren@users.noreply.github.com>
2021-03-18 06:27:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='Wiktra',
+   name='',
    version='1.0',
    description='Wiktionary Transiltration tool',
    author='Khuyagbaatar Batsuren',
    author_email='khuyagbaatar@gmail.com',
-   packages=['Wiktra'],  #same as name
+   packages=[''],  #same as name
    install_requires=[], #external packages as dependencies
 )
